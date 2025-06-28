5cfe2e6c1ac8cc13549f16e5372f6c0b3935a648
Aiigeriim <sardarbekova1005@gmail.com>
2025-06-28 22:57:19 +0300

    назвала роуты

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -3,8 +3,9 @@ from django.urls import path
 from webapp.views import index, create_task, task_detail
 
 urlpatterns = [
-    path('', index),
-    path('add-task/', create_task),
-    path('task/', task_detail)
+    path('', index, name='index'),
+    path('add-task/', create_task, name='create_task'),
+    path('task/<int:pk>', task_detail, name='task_detail'),
 ]
+
 
