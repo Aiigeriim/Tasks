cb369e0dcdeef93cecab2bdc7da207657ddfa80d
Aiigeriim <sardarbekova1005@gmail.com>
2025-07-02 23:21:32 +0300

    Добавила удаление и редактирование

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from webapp.views import index, create_task, task_detail
+from webapp.views import index, create_task, detail_task, update_task, delete_task
 
 urlpatterns = [
     path('', index, name='index'),
     path('add-task/', create_task, name='create_task'),
-    path('task/<int:pk>', task_detail, name='task_detail'),
+    path('task/<int:pk>/', detail_task, name='detail_task'),
+    path('task/<int:pk>/update/', update_task, name='update_task'),
+    path('task/<int:pk>/delete/', delete_task, name='delete_task'),
+
 ]
-
-
+delete_task
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -13,16 +13,44 @@ def create_task(request):
         name = request.POST.get('name')
         description = request.POST.get('description')
         status = request.POST.get('status')
-        completion_date = request.POST.get('myDate')
-
+        completion_date = request.POST.get('mydate')
         task = Task.objects.create(name=name, description=description, status=status, completion_date=completion_date)
-        return redirect('task_detail', pk=task.pk)
+        return redirect('detail_task', pk=task.pk)
     else:
         return render(request, 'create_task.html', {"status_choices": status_choices})
 
+def delete_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == "POST":
+        task.delete()
+        return redirect('index')
+    else:
+        context = {
+            'task': task,
+            'status_choices': status_choices,
+        }
+        return render(request, 'delete_task.html', context)
 
 
-def task_detail(request, *args, pk, **kwargs):
+
+def update_task(request, *args, pk, **kwargs):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == "POST":
+        task.name = request.POST.get('name')
+        task.description = request.POST.get('description')
+        task.status = request.POST.get('status')
+        task.completion_date = request.POST.get('mydate')
+        task.save()
+        return redirect('detail_task', pk=task.pk)
+    else:
+        context = {
+            'task': task,
+            'status_choices': status_choices,
+        }
+        return render(request, 'update_task.html', context)
+
+
+def detail_task(request, *args, pk, **kwargs):
     task = get_object_or_404(Task, id=pk)
     return render(request, 'detail_task.html', {"task": task})
 
