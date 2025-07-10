fc1c68e27017b438aee177d81dad76d992b750da
Aiigeriim <sardarbekova1005@gmail.com>
2025-07-10 11:44:59 +0300

    добавила поле published_at в модель задачи

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -7,7 +7,7 @@ class Task(models.Model):
     description = models.TextField(verbose_name='Описание', null=True, blank=True)
     status = models.CharField(max_length=15, choices=status_choices, default='new')
     completion_date = models.DateField(auto_now=False, null=True, blank=True, verbose_name='Дэдлайн')
-
+    published_at = models.DateTimeField(null=True, blank=True, verbose_name='Дата публикации')
 
     def __str__(self):
         return f"{self.id} - {self.name}"
