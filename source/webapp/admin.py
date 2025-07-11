809618a86b28fd9ea7fc11674b778dca5fffd099
Aiigeriim <sardarbekova1005@gmail.com>
2025-07-11 03:51:05 +0300

    удалила миграции, накатила новую

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,14 +1,31 @@
 from django.contrib import admin
-from webapp.models import Task
+from webapp.models import Task, TaskStatus, TaskType
 
 
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'completion_date']
-    list_display_links = ['id', 'name']
-    list_filter = ['completion_date', 'status']
-    search_fields = ['name']
-    fields = ['name', 'status', 'completion_date', 'description' ]
+    list_display = ['id', 'summary', 'status']
+    list_display_links = ['id', 'summary']
+    list_filter = ['status']
+    search_fields = ['summary']
+    fields = ['summary', 'status', 'type', 'description' ]
     #readonly_fields = ['completion_date']
 
 
 admin.site.register(Task, TaskAdmin)
+
+class TaskStatusAdmin(admin.ModelAdmin):
+    list_display = ["id", 'name']
+    list_display_links = ['id', 'name']
+    search_fields = ['name']
+    fields = ['name']
+
+
+admin.site.register(TaskStatus, TaskStatusAdmin)
+
+class TaskTypeAdmin(admin.ModelAdmin):
+    list_display = ["id", 'name']
+    list_display_links = ['id', 'name']
+    search_fields = ['name']
+    fields = ['name']
+
+admin.site.register(TaskType, TaskTypeAdmin)
--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,42 +1,42 @@
-from django import forms
-from django.core.exceptions import ValidationError
-from django.forms import widgets
-from django.utils import timezone
+# from django import forms
+# from django.core.exceptions import ValidationError
+# from django.forms import widgets
+# from django.utils import timezone
 
-from webapp.models import status_choices, Task, Tag
+# from webapp.models import Task
 
 
-class TaskForm(forms.ModelForm):
-    def __init__(self, *args, **kwargs):
-        super(TaskForm, self).__init__(*args, **kwargs)
-        for field in self.fields.values():
-            field.widget.attrs['class'] = 'form-control'
+# class TaskForm(forms.ModelForm):
+    # def __init__(self, *args, **kwargs):
+    #     super(TaskForm, self).__init__(*args, **kwargs)
+    #     for field in self.fields.values():
+    #         field.widget.attrs['class'] = 'form-control'
 
-    class Meta:
-        model = Task
-        # exclude = ('completion_date', 'published_at')
-        fields = ('name', 'description', 'status', 'completion_date', 'published_at', 'tags')
-        widgets = {
-            'published_at':  widgets.DateTimeInput(
-                attrs={'type': 'datetime-local'}),
-            'completion_date': widgets.DateInput(
-                attrs={'type': 'date'}),
-            'tags': forms.ModelChoiceField(),
-            'comments': forms.Textarea(),
-        }
-        error_messages = {'title': {"required": "Пожалуйста, введите название"}}
+    # class Meta:
+    #     model = Task
+    #     # exclude = ('completion_date', 'published_at')
+    #     fields = ('name', 'description', 'status', 'completion_date', 'published_at', 'tags')
+    #     widgets = {
+    #         # 'published_at':  widgets.DateTimeInput(
+    #         #     attrs={'type': 'datetime-local'}),
+    #         'completion_date': widgets.DateInput(
+    #             attrs={'type': 'date'}),
+    #         # 'tags': forms.ModelChoiceField(),
+            # 'comments': forms.Textarea(),
+        # }
+        # error_messages = {'title': {"required": "Пожалуйста, введите название"}}
 
-    def clean_published_at(self):
-        published_at = self.cleaned_data['published_at']
-        if not published_at:
-            raise ValidationError("заполните это поле")
-        if published_at < timezone.now():
-            raise ValidationError("This date is in the future")
-        return published_at
+    # def clean_published_at(self):
+    #     published_at = self.cleaned_data['published_at']
+    #     if not published_at:
+    #         raise ValidationError("заполните это поле")
+    #     if published_at < timezone.now():
+    #         raise ValidationError("This date is in the future")
+    #     return published_at
 
-    def clean(self):
-        name = self.cleaned_data.get('name')
-        description = self.cleaned_data.get('description')
-        if name and description and  name == description:
-            raise ValidationError("Название и описание не могут быть одинаковыми")
-        return super(TaskForm, self).clean()
+    # def clean(self):
+    #     name = self.cleaned_data.get('name')
+    #     description = self.cleaned_data.get('description')
+    #     if name and description and  name == description:
+    #         raise ValidationError("Название и описание не могут быть одинаковыми")
+    #     return super(TaskForm, self).clean()
