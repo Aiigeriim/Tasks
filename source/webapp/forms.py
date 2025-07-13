1021c7cae637a669688a1943a6a60c6ee5908f2d
Aiigeriim <sardarbekova1005@gmail.com>
2025-07-14 01:10:54 +0300

    реализовала редактирование с использованием formview, оптимизировала логику модельной формы

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -1,27 +1,34 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from django.forms import widgets
 from webapp.models import Task, TaskType
 
 
 class TaskForm(forms.ModelForm):
-    type = forms.ModelMultipleChoiceField(
-        queryset=TaskType.objects.all(),
-        widget=forms.CheckboxSelectMultiple()
-    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for v in self.visible_fields():
+            if not isinstance(v.field.widget, widgets.CheckboxSelectMultiple):
+                v.field.widget.attrs['class'] = 'form-control'
 
     class Meta:
         model = Task
         fields = ['summary', 'description', 'status', 'type']
         widgets = {
-            'summary': forms.TextInput(attrs={'class': 'form-control'}),
-            'description': forms.Textarea(attrs={'class': 'form-control'}),
-            'status': forms.Select(attrs={'class': 'form-control'}),
+            'type': forms.CheckboxSelectMultiple()
         }
         error_messages = {'summary': {"required": "Пожалуйста, введите название"}}
 
+    def clean_summary(self):
+        summary = self.cleaned_data['summary']
+        if len(summary) < 5:
+            raise ValidationError("Это поле должно состоять из более 5 символов")
+        return summary
+
     def clean(self):
-        summary = self.cleaned_data.get('name')
+        summary = self.cleaned_data.get('summary')
         description = self.cleaned_data.get('description')
         if summary and description and  summary == description:
-            raise ValidationError("Название и описание не могут быть одинаковыми")
+            raise ValidationError("Описания не могут быть одинаковыми")
         return super(TaskForm, self).clean()
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -17,35 +17,37 @@ class IndexView(TemplateView):
 
 class CreateTaskView(FormView):
     template_name = 'create_task.html'
-    # success_url = reverse_lazy('index')
     form_class = TaskForm
-
-    # def get_success_url(self):
-    #     return reverse('detail_task', kwargs={'pk': self.task.pk})
-    #     # return f'task/{self.task.pk}/'
-
-    def form_invalid(self, form):
-        pass
 
     def form_valid(self, form):
         task = form.save()
-        task.type.set(form.cleaned_data['type'])
         return  redirect('detail_task', pk=task.pk)
 
 
-        # self.task = task
-        # return super().form_valid(form)
+class UpdateTaskView(FormView):
+    template_name = 'update_task.html'
+    form_class = TaskForm
 
+    def dispatch(self, request, *args, **kwargs):
+        self.task = self.get_object()
+        return super().dispatch(request, *args, **kwargs)
 
-    def post(self, request):
-        form = TaskForm(request.POST)
-        if form.is_valid():
-            task = form.save()
-            task.type.set(form.cleaned_data['type'])
-            return redirect('detail_task', pk=task.pk)
-        else:
-            return render(request, 'create_task.html', {'form': form})
+    def get_object(self):
+        return get_object_or_404(Task, pk=self.kwargs['pk'])
 
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['instance'] = self.task
+        return kwargs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context['task'] = self.task
+        return context
+
+    def form_valid(self, form):
+        form.save()
+        return redirect('detail_task', pk=self.task.pk)
 
 class DeleteTaskView(View):
     def post(self, request, pk):
@@ -57,22 +59,6 @@ class DeleteTaskView(View):
         return render(request, 'delete_task.html', {'pk': pk})
 
 
-class UpdateTaskView(View):
-    def post(self, request, pk):
-        task = get_object_or_404(Task, pk=pk)
-        form = TaskForm(request.POST, instance=task)
-        if form.is_valid():
-            task = form.save()
-            task.type.set(form.cleaned_data['type'])
-            return redirect('detail_task', pk=task.pk)
-        else:
-            return render(request, 'update_task.html', {'form': form})
-
-
-    def get(self, request, pk):
-        task = get_object_or_404(Task, pk=pk)
-        form = TaskForm(instance=task)
-        return render(request, 'update_task.html', {'form': form})
 
 class DetailTaskView(TemplateView):
     template_name = 'detail_task.html'
@@ -85,12 +71,6 @@ class DetailTaskView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['task'] = self.task
         return context
-
-    # def get_template_names(self):
-    #     if self.task.status:
-    #         return ['detail_task.html']
-    #     else:
-    #         return ['detail_task.html']
 
 def delete_all_tasks(request):
     ids = request.POST.getlist('task_ids')
