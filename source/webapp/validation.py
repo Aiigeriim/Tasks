0d24cab4ec6c91fbbbcd4f508f9b65f39477561a
Aiigeriim <sardarbekova1005@gmail.com>
2025-07-22 00:49:32 +0300

    реализовала постраничный вывод задач с поиском

--- source/webapp/validation.py
+++
@@ -1,8 +0,0 @@
-def validate(data):
-    errors = {}
-    name = data.get('name')
-    if not name:
-        errors['name'] = 'Пожалуйста, заполните это поле'
-    elif len(name) < 3:
-        errors['name'] = 'Пожалуйста, заполните это поле на более 3 символов'
-    return errors
--- source/webapp/views/tasks.py
+++ source/webapp/views/tasks.py
@@ -1,18 +1,54 @@
+
+
+from django.db.models import Q
 from django.shortcuts import render, redirect, get_object_or_404
-from django.urls import reverse, reverse_lazy
+from django.utils.http import urlencode
 from django.views import View
-from django.views.generic import TemplateView, FormView
-from webapp.forms import TaskForm
+from django.views.generic import TemplateView, FormView, ListView
+from webapp.forms import TaskForm, SearchForm
 from webapp.models import Task
 
+class TaskListView(ListView):
+    template_name = "tasks/index.html"
+    model = Task
+    context_object_name = "tasks"
+    ordering = ("-created_at")
+    paginate_by = 2
+    paginated_orphans = 1
 
-class TaskListView(TemplateView):
-    template_name = 'tasks/index.html'
+    def dispatch(self, request, *args, **kwargs):
+        self.form = self.get_search_form()
+        self.search_value = self.get_search_value()
+        return super().dispatch(request, *args, **kwargs)
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['tasks'] = Task.objects.order_by('-created_at')
-        return context
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.search_value:
+            queryset = queryset.filter(Q(summary__icontains=self.search_value) | Q(status__name__icontains=self.search_value))
+        return queryset
+
+    def get_context_data(self, *, object_list = None, **kwargs):
+        result = super().get_context_data(**kwargs)
+        result['search_form'] = self.form
+        if self.search_value:
+            result['query'] = urlencode({'search': self.search_value})
+            result['search'] = self.search_value
+        return result
+
+    def get_search_form(self):
+        return SearchForm(self.request.GET)
+
+    def get_search_value(self):
+        if self.form.is_valid():
+            return self.form.cleaned_data["search"]
+
+# class TaskListView(TemplateView):
+#     template_name = 'tasks/index.html'
+#
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         context['tasks'] = Task.objects.order_by('-created_at')
+#         return context
 
 
 class CreateTaskView(FormView):
@@ -57,7 +93,6 @@ class DeleteTaskView(View):
 
     def get(self, request, pk):
         return render(request, 'tasks/delete_task.html', {'pk': pk})
-
 
 
 class DetailTaskView(TemplateView):
